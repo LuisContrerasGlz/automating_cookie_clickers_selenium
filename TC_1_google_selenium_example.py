1d038d8546cfe22d38c56292445429ff19f180d7
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-11-30 13:03:55 -0600

    progress

--- TC_1_google_selenium_example.py
+++ TC_1_google_selenium_example.py
@@ -6,26 +6,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+# Set up Chrome WebDriver with the specified executable path
 service = Service(executable_path="drivers/chromedriver")
 driver = webdriver.Chrome(service=service)
 
+# Open Google's homepage and maximize the browser window
 driver.get("https://google.com")
 driver.maximize_window()
 
-WebDriverWait(driver, 2).until(
+# Wait for up to 10 seconds for the search input element to be present
+input_element = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
 )
 
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+# Clear any existing text in the input field and type the search query and press Enter
 input_element.clear()
 input_element.send_keys("Darth Vader" + Keys.ENTER)
 
-WebDriverWait(driver, 2).until(
+# Wait for up to 10 seconds for a search result link containing "Darth Vader" to be present
+link = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Darth Vader"))
 )
-link = driver.find_element(By.PARTIAL_LINK_TEXT, "Darth Vader")
-link.click()
 
-time.sleep(5)
+link.click()
 
 driver.quit()
