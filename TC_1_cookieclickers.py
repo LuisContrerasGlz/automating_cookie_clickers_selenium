d5325fb337d230259b3351d0ae95a8ed59811206
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-11-30 16:40:12 -0600

    progress

--- TC_1_cookieclickers.py
+++ TC_1_cookieclickers.py
@@ -1,21 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Set up Chrome WebDriver with the specified executable path
 service = Service(executable_path="drivers/chromedriver")
 driver = webdriver.Chrome(service=service)
-
-# Open page and maximize the browser window
-driver.get("https://orteil.dashnet.org/cookieclicker/")
 driver.maximize_window()
-driver.implicitly_wait(10)
 
-lang_selector = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
-lang_selector.click
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-driver.implicitly_wait(10)
+# Wait for the "English" language element to be clickable
+try:
+    WebDriverWait(driver, 3).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'English')]"))
+    ).click()
+except Exception as e:
+    print(f"Error clicking on language: {e}")
+
+cookie_id = "bigCookie"
+cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
+
+# Wait for the big cookie to be present
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, cookie_id))
+    )
+except Exception as e:
+    print(f"Error waiting for big cookie: {e}")
+
+while True:
+    try:
+        cookie = driver.find_element(By.ID, cookie_id)
+        cookie.click()
+        
+        # Get the current number of cookies
+        cookies_count = int(driver.find_element(By.ID, cookies_id).text.split(" ")[0].replace(",", ""))
+        
+        for i in range(4):
+            product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+            if not product_price.isdigit():
+                continue
+
+            product_price = int(product_price)
+
+            if cookies_count >= product_price:
+                product = driver.find_element(By.ID, product_prefix + str(i))
+                product.click()
+                break
+    except Exception as e:
+        print(f"Error during interaction: {e}")
