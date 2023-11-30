9ef1385c977fc5c7b6dacdfbab6d87dd9651465e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-11-29 20:04:20 -0600

    progress

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(executable_path="drivers/chromedriver")
@@ -9,7 +11,12 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
 input_element.send_keys("Darth Vader" + Keys.ENTER)
 
 time.sleep(5)
