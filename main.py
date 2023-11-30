e3cea25cb62d283987c6a0ea80c827b6e3e58b5f
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-11-29 19:48:54 -0600

    progress

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 service = Service(executable_path="drivers/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("Darth Vader")
 
 time.sleep(10)
 
